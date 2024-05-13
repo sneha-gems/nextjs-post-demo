9f0f0ce7f302bcb28fa9472f9f4782d956efff7d
sneha-itaims <sneha.sawla@gemsessence.com>
2024-05-13 20:25:02 +0530

    small updates

--- src/app/createPost/layout.jsx
+++ src/app/createPost/layout.jsx
@@ -1,13 +1,15 @@
 'use client'
 export default function Layout({ children }) {
     return (
-        <>
+        <html>
+            <body>
         <div class="heading text-center font-bold text-2xl m-5 text-gray-800">New Post</div>
         <div class="editor mx-auto w-10/12 flex flex-col text-gray-800 border border-gray-300 p-4 shadow-lg max-w-2xl">
               
   
               {children}
             </div>
-          </>
+            </body>
+          </html>
     );
   }
